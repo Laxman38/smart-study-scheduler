627cdafcd1e70b42f26da2945d61d44d5a461425
*Laxman38* <*Laxmansinghgehloth9206*>
2025-08-27 19:54:51 +0530

    Deployed project

--- backend/server.js
+++ backend/server.js
@@ -4,22 +4,24 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
 app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/user', require('./routes/UserRoutes'));
+app.use('/api/user', require('./routes/userRoutes'));
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/progress', require('./routes/progressRoutes'));
 app.use('/api/tasks', require('./routes/taskRoutes'));
 app.use('/api/settings', require('./routes/settingsRoutes'));
 
-mongoose.connect(process.env.MONGO_URI, { 
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log('MongoDB connected');
-    app.listen(process.env.PORT, () => {
+
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
         console.log(`Server running on port ${process.env.PORT}`);
     });
 })
--- src/pages/Calendar.jsx
+++
@@ -1,9 +0,0 @@
-import CalendarView from "../components/CalendarView";
-
-function Calendar() {
-    return (
-        <CalendarView />
-    );
-}
-
-export default Calendar;
